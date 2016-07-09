60fad69a6ed92a74a3a6b98ba11c38a58732b05c
jaga <jagamongol@gmail.com>
2016-07-09 04:41:40 +0000

    quotes section in work

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,9 +1,24 @@
-$(function() {
-    smoothScroll(1000);
+/*global $ */
+
+$(document).ready(function() {
+    smoothScroll(700);
+    
+    $("h1").fitText(1, { minFontSize: '20px', maxFontSize: '64px' });
+    
+    setInterval(function(){articleTada()}, 4000);
+    
 });
 
+function articleTada(){
+    var randNum = Math.floor(Math.random() * $('.picture-container').length) + 1;
+    
+    $('.pictures').eq(randNum).addClass('is-emph');
+    $('.pictures').siblings().removeClass('is-emph');
+}
+
+
 function smoothScroll (duration) {
-    $('a[href^="#]').on('click', function(event) {
+    $('a[href^="#"]').on('click', function(event) {
         
         var target = $( $(this).attr('href') );
         
@@ -16,3 +31,36 @@ function smoothScroll (duration) {
             });
 }
 
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( $ );
