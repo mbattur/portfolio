4ad02484dece4fb38322055293cbde71c517079a
jaga <jagamongol@gmail.com>
2016-07-10 19:35:10 +0000

    dropdown maybe

--- scripts/landing.js
+++ scripts/landing.js
@@ -72,3 +72,17 @@ function smoothScroll (duration) {
 function myFunction() {
     document.getElementById('myDropdown').classList.toggle('show');
 }
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
