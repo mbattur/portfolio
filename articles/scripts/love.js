02c56f827665fbdd0ab5822501a9f19a515c14f9
jaga <jagamongol@gmail.com>
2016-07-14 01:25:43 +0000

    trying love article

--- articles/scripts/love.js
+++ articles/scripts/love.js
@@ -1,3 +1,6 @@
+/*global $ */ 
+
+
 var linesArray = document.getElementsByClassName('line');
 
 var animateLines = function(lines) {
@@ -7,20 +10,52 @@ var animateLines = function(lines) {
         lines[index].style.opacity = 1;
     }
     
-    for (var i = 0; i < lines.length; i++) {
+ for (var i = 0; i < lines.length; i++) {
         revealLine(i);
-    }
+   }
 };
 
 window.onload = function() {
     
-    var scrollHead = document.getElementsByClassName('head')[0];
-    var scrollDistance = scrollHead.getBoundingClientRect().top - window.innerHeight + 200;
+    
+  var revealLine = window.innerHeight;
+    
     
     window.addEventListener('scroll', function(event) {
 
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= revealLine) {
         animateLines(linesArray);
     }
     });
 }
+
+
+
+/*
+var $animation_elements = $('.line');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+*/
