b3417d87a7118b4d5c925d2da942323cee6c2558
jaga <jagamongol@gmail.com>
2016-07-14 04:55:58 +0000

    love article

--- articles/scripts/love.js
+++ articles/scripts/love.js
@@ -1,61 +1,26 @@
 /*global $ */ 
 
-
-var linesArray = document.getElementsByClassName('line');
-
-var animateLines = function(lines) {
+$(document).ready(function() {
     
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
     
-    var revealLine = function(index) {
-        lines[index].style.opacity = 1;
-    }
+        /* Check the location of each desired element */
+        $('.line').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight() + 250;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
     
- for (var i = 0; i < lines.length; i++) {
-        revealLine(i);
-   }
-};
-
-window.onload = function() {
-    
-    
-  var revealLine = window.innerHeight;
-    
-    
-    window.addEventListener('scroll', function(event) {
-
-    if (document.documentElement.scrollTop || document.body.scrollTop >= revealLine) {
-        animateLines(linesArray);
-    }
     });
-}
+    
+});
 
-
-
-/*
-var $animation_elements = $('.line');
-var $window = $(window);
-
-function check_if_in_view() {
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
- 
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
- 
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-        (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
-    } else {
-      $element.removeClass('in-view');
-    }
-  });
-}
-
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
-*/
