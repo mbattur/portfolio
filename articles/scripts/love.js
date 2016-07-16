c5e533f2666e59ae35a97b895682b6e6f6fc7d5a
jaga <jagamongol@gmail.com>
2016-07-16 03:35:27 +0000

    class add

--- articles/scripts/love.js
+++ articles/scripts/love.js
@@ -1,6 +1,8 @@
 /*global $ */ 
 
+
 $(document).ready(function() {
+    
     
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
@@ -11,16 +13,27 @@ $(document).ready(function() {
             var bottom_of_object = $(this).offset().top + $(this).outerHeight() + 250;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            /* If the object is completely visible in the window, fade it it */
+            
+            /* If the object is completely visible in the window, show it and ad black class */
             if( bottom_of_window > bottom_of_object ){
                 
-                $(this).animate({'opacity':'1'},500);
-                    
+                $(this).animate({'opacity':'1'},500).addClass('black');
+                $(this).siblings().removeClass('black');
+                
+                
             }
-            
         }); 
     
     });
     
+    
 });
+
+
+
+(function blink() { 
+  $('.blink').fadeOut(500).fadeIn(500, blink); 
+})();
+
+
 
