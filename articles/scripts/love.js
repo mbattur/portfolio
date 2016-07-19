50d781798a63f8a92d1191d1a47a1b37079728f2
jaga <jagamongol@gmail.com>
2016-07-19 15:47:09 +0000

    msg

--- articles/scripts/love.js
+++ articles/scripts/love.js
@@ -1,5 +1,5 @@
 /*global $ */ 
-
+var lines = $('.line');
 
 $(document).ready(function() {
     
@@ -7,10 +7,10 @@ $(document).ready(function() {
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
         
-        var lines = $('.line');
-    
+        //var lines = $('.line');
         /* Check the location of each desired element */
         lines.each( function(i){
+            
             
             var bottom_of_object = $(this).offset().top + $(this).outerHeight() + 250;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -19,13 +19,11 @@ $(document).ready(function() {
             /* If the object is completely visible in the window, show it and ad black class */
             if( bottom_of_window > bottom_of_object ){
                 
+                
                 $(this).animate({'opacity':'1'},500).addClass('black');
                 $(this).siblings().removeClass('black');
                 
             }
-            
-            
-        
         }); 
     
     });
@@ -35,10 +33,11 @@ $(document).ready(function() {
 //$('.line[class="black"]').append(<span>wow</span>);
 
 
-
 (function blink() { 
   $('.blink').fadeOut(500).fadeIn(500, blink); 
 })();
+/*
+lines.append('<span>blink</span>');
+lines.siblings().remove('<span>wow</span>');
 
-
-
+*/
