085dd4721616be7520c412224125a34e0243a7f5
jaga <jagamongol@gmail.com>
2016-07-30 21:20:27 +0000

    quotes done

--- articles/scripts/quotes.js
+++ articles/scripts/quotes.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
     // FOOTER JS
-    $(".back-to-article").hide();
+   $(".back-to-article").hide();
         
         $(".ps").mouseenter(function() {
             $(".back-to-article").slideDown(400);
@@ -16,5 +16,69 @@ $(document).ready(function() {
         });
         
         
+        
+        /*$('.start').click( function() {
+           var currentPage = $('.current');
+           var nextPage = currentPage.next();
+           
+           var currentLine = $('.active');
+           var nextLine = currentLine.next();
+           
+           if(nextPage.length === 0) {
+               nextPage = $('.page').first();
+               nextLine = $('.line').first();
+       }
+       
+       currentPage.fadeOut(500).removeClass('current');
+       nextPage.fadeIn(600).addClass('current');
+       
+       currentLine.removeClass('active');
+       nextLine.addClass('active');
+       
+           
+        });*/
+        
+        
+        $('.next').click( function() {
+           var currentPage = $('.current');
+           var nextPage = currentPage.next();
+           
+           var currentLine = $('.active');
+           var nextLine = currentLine.next();
+           
+           if(nextPage.length === 0) {
+               nextPage = $('.page').first();
+               nextLine = $('.line').first();
+       }
+       
+       currentPage.slideUp(800).removeClass('current');
+       nextPage.slideDown(800).addClass('current');
+       
+       currentLine.removeClass('active');
+       nextLine.addClass('active');
+       
+        });
+        
+        
+        $('.prev').click( function() {
+           var currentPage = $('.current');
+           var prevPage = currentPage.prev();
+           
+           var currentLine = $('.active');
+           var prevLine = currentLine.prev();
+           
+           if(prevPage.length === 0) {
+               prevPage = $('.page').last();
+               prevLine = $('.line').last();
+       }
+       
+       currentPage.slideUp(800).removeClass('current');
+       prevPage.slideDown(800).addClass('current');
+       
+       currentLine.removeClass('active');
+       prevLine.addClass('active');
+       
+    });
+    
 });
 
